d9ab087d6f93069ddfbdc4fe0ea58079cf603bf8
islamov-tema <islamov.tema@yandex.com>
2022-09-27 15:51:52 +0500

    add: send form to telegram bot

--- handler.php
+++ handler.php
@@ -1,5 +1,28 @@
 <?php 
-$user_name = $_POST["username"];
-$user_phone = $_POST['userphone'];
-echo "Привет, " . $user_name . "<br>";
-echo "Ваш телефон: <b>" . $user_phone . "</b>";
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+$user_name = htmlspecialchars($_POST["username"]);
+$user_phone = htmlspecialchars($_POST['userphone']);
+
+$token = "5684109873:AAGgAp3bujo3LgxkYJx0r6CBy0XQ3qHSw2c";
+$chat_id = "-863459208";
+$text = "";
+
+$formData = array(
+  "Клиент: " => $user_name, 
+  "Телефон: " => $user_phone
+);
+
+foreach($formData as $key => $value) {
+  $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A" ;
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
+
+if ($sendToTelegram) {
+  echo "ok";
+} else {
+  echo "Error";
+}
+
