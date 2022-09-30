0523092e806eca5d4f50b9d1c0805f0970e0abbb
islamov-tema <islamov.tema@yandex.com>
2022-09-30 12:50:36 +0500

    add: php-template-parts

--- index.php
+++ index.php
@@ -351,38 +351,8 @@
       </div>
       <!-- /.container -->
     </section>
-    <section class="section trademark section-trademark">
-      <div class="container">
-        <div class="seporator"></div>
-        <h2 class="section-title">Собственные торговые марки</h2>
-        <div class="trademark-cards">
-          <div class="trademark-card">
-            <svg class="trademark-card-logo">
-              <use href="img/sprite.svg#AGTECH"></use>
-            </svg>
-            <h3 class="trademark-card-title">Автохимия AG-Tech</h3>
-            <p class="trademark-card-text">
-              Для современного мира разбавленное изрядной долей эмпатии,
-              рациональное мышление создаёт предпосылки для поставленных
-              обществом задач.
-            </p>
-          </div>
-          <div class="trademark-card">
-            <svg class="trademark-card-logo">
-              <use href="img/sprite.svg#AP"></use>
-            </svg>
-            <h3 class="trademark-card-title">Автохимия AP</h3>
-            <p class="trademark-card-text">
-              Для современного мира разбавленное изрядной долей эмпатии,
-              рациональное мышление создаёт предпосылки для поставленных
-              обществом задач.
-            </p>
-          </div>
-        </div>
-        <!-- /.trademark-cards -->
-      </div>
-      <!-- /.container -->
-    </section>
+    <?php $block_title = "собственные торговые марки"; 
+    include_once('./template-parts/trademarks-block.php')?>
     <section class="section founder section-founder">
       <img src="img/founder.jpg" alt="founder" class="founder-photo" />
       <div class="container">
